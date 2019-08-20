e1d84c90890650607495f48fbba85ff6237984e1
saadat-atta-brainx <saadat.atta@brainxtech.com>
2019-08-20 15:48:59 +0500

    User signup process complete

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,8 +1,25 @@
 class RegistrationsController < Devise::RegistrationsController
+    
+    def new
+        @user = User.new
+    end
 
-    # def new
+    def create
+        @user = User.new(user_params)
+        # render plain: params[:user].inspect
+        if(@user.save)
+            redirect_to :new_user_session
+        else
+            render 'new'
+        end
+    end
 
-    # end
+    private
+
+    def user_params
+        # devise_parameter_sanitizer.for(:sign_up){|u|u.permit(:email,:password,:password_confirmation)}
+        params.require(:user).permit(:username,:email,:password,:password_confirmation)
+    end
 
 
 end
